8f9c4e45417663aaccbe327f52dab5098684ce00
Fernando Gnadt de Oliveira <fernando.oliveira@mobilus.com.br>
2023-05-10 23:03:01 -0300

    aula 6

--- lib/features/genre_list/presentation/bindings/genre_list_bindings.dart
+++ lib/features/genre_list/presentation/bindings/genre_list_bindings.dart
@@ -1,9 +1,17 @@
 import 'package:get/get.dart';
+import 'package:music_app_flutter/core/services/api_service.dart';
+import 'package:music_app_flutter/features/genre_list/presentation/controllers/data/repositories/genre_list_repository.dart';
 import 'package:music_app_flutter/features/genre_list/presentation/controllers/genre_list_controller.dart';
 
 class GenreListBinding implements Bindings {
   @override
   void dependencies() {
-    Get.put<GenreListController>(GenreListController());
+    Get.put<GenreListController>(
+      GenreListController(
+        GenreListRepository(
+          apiService: Get.find<ApiService>(),
+        ),
+      ),
+    );
   }
 }
--- lib/features/genre_list/presentation/screens/genre_list_screen.dart
+++ lib/features/genre_list/presentation/screens/genre_list_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:music_app_flutter/features/genre_details/presentation/screens/genre_details_screen.dart';
+import 'package:music_app_flutter/features/genre_list/presentation/controllers/genre_list_controller.dart';
 import 'package:music_app_flutter/shared/widgets/screen_widget.dart';
+
+import '../../../../shared/widgets/img_and_title_row_widget.dart';
 
 class GenreListScreen extends StatelessWidget {
   static const routeName = '/genre-list';
@@ -7,10 +12,42 @@ class GenreListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenWidget(
-      isLoading: false,
-      title: 'Lista de Gêneros',
-      child: Container(),
+    final genreListContrl = Get.find<GenreListController>();
+    return Obx(
+      () => ScreenWidget(
+        isLoading: false,
+        title: 'Lista de Gêneros',
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: ListView.separated(
+                  itemBuilder: (_, int index) {
+                    final genre = genreListContrl.genres[index];
+                    return InkWell(
+                      onTap: () => Get.toNamed(
+                        '${GenreListScreen.routeName}${GenreDetailsScreen.routeName}',
+                        arguments: genre,
+                      ),
+                      child: ImgAndTitleRowWidget(
+                        title: genre.title,
+                        heroTag: genre.title,
+                        img: genre.img,
+                      ),
+                    );
+                  },
+                  separatorBuilder: (_, __) {
+                    return const SizedBox(height: 12);
+                  },
+                  itemCount: genreListContrl.genres.length,
+                ),
+              ),
+            ),
+            const SizedBox(height: 12),
+          ],
+        ),
+      ),
     );
   }
 }
